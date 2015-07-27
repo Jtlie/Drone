7fb44764203add9a0324012e15462c73185e76df
Hoye <hoyelam96@gmail.com>
2015-07-27 14:57:02 +0200

    extra commit

--- cvdrone-master/src/main.cpp
+++ cvdrone-master/src/main.cpp
@@ -16,52 +16,68 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
-	// Battery
-	std::cout << "Battery = " << ardrone.getBatteryPercentage() << "[%]" << std::endl;
+	// Thresholds
+	int minH = 0, maxH = 255;
+	int minS = 0, maxS = 255;
+	int minV = 0, maxV = 255;
 
-	// Map
-	cv::Mat map = cv::Mat::zeros(500, 500, CV_8UC3);
+	// XML save data
+	std::string filename("thresholds.xml");
+	cv::FileStorage fs(filename, cv::FileStorage::READ);
+
+	// If there is a save file then read it
+	if (fs.isOpened()) {
+		maxH = fs["H_MAX"];
+		minH = fs["H_MIN"];
+		maxS = fs["S_MAX"];
+		minS = fs["S_MIN"];
+		maxV = fs["V_MAX"];
+		minV = fs["V_MIN"];
+		fs.release();
+	}
+
+	// Create a window
+	cv::namedWindow("binalized");
+	cv::createTrackbar("H max", "binalized", &maxH, 255);
+	cv::createTrackbar("H min", "binalized", &minH, 255);
+	cv::createTrackbar("S max", "binalized", &maxS, 255);
+	cv::createTrackbar("S min", "binalized", &minS, 255);
+	cv::createTrackbar("V max", "binalized", &maxV, 255);
+	cv::createTrackbar("V min", "binalized", &minV, 255);
+	cv::resizeWindow("binalized", 0, 0);
 
 	// Kalman filter
-	cv::KalmanFilter kalman(6, 4, 0);
+	cv::KalmanFilter kalman(4, 2, 0);
 
 	// Sampling time [s]
-	const double dt = 0.033;
+	const double dt = 1.0;
 
-	// Transition matrix (x, y, z, vx, vy, vz)
-	cv::Mat1f F(6, 6);
-	F << 1.0, 0.0, 0.0, dt, 0.0, 0.0,
-		0.0, 1.0, 0.0, 0.0, dt, 0.0,
-		0.0, 0.0, 1.0, 0.0, 0.0, dt,
-		0.0, 0.0, 0.0, 1.0, 0.0, 0.0,
-		0.0, 0.0, 0.0, 0.0, 1.0, 0.0,
-		0.0, 0.0, 0.0, 0.0, 0.0, 1.0;
-	kalman.transitionMatrix = F;
+	// Transition matrix (x, y, vx, vy)
+	cv::Mat1f A(4, 4);
+	A << 1.0, 0.0, dt, 0.0,
+		0.0, 1.0, 0.0, dt,
+		0.0, 0.0, 1.0, 0.0,
+		0.0, 0.0, 0.0, 1.0;
+	kalman.transitionMatrix = A;
 
-	// Measurement matrix (0, 0, z, vx, vy, vz)
-	cv::Mat1f H(4, 6);
-	H << 0, 0, 1, 0, 0, 0,
-		0, 0, 0, 1, 0, 0,
-		0, 0, 0, 0, 1, 0,
-		0, 0, 0, 0, 0, 1;
+	// Measurement matrix (x, y)
+	cv::Mat1f H(2, 4);
+	H << 1, 0, 0, 0,
+		0, 1, 0, 0;
 	kalman.measurementMatrix = H;
 
-	// Process noise covariance (x, y, z, vx, vy, vz)
-	cv::Mat1f Q(6, 6);
-	Q << 0.1, 0.0, 0.0, 0.0, 0.0, 0.0,
-		0.0, 0.1, 0.0, 0.0, 0.0, 0.0,
-		0.0, 0.0, 0.1, 0.0, 0.0, 0.0,
-		0.0, 0.0, 0.0, 0.3, 0.0, 0.0,
-		0.0, 0.0, 0.0, 0.0, 0.3, 0.0,
-		0.0, 0.0, 0.0, 0.0, 0.0, 0.3;
+	// Process noise covariance (x, y, vx, vy)
+	cv::Mat1f Q(4, 4);
+	Q << 1e-5, 0.0, 0.0, 0.0,
+		0.0, 1e-5, 0.0, 0.0,
+		0.0, 0.0, 1e-5, 0.0,
+		0.0, 0.0, 0.0, 1e-5;
 	kalman.processNoiseCov = Q;
 
-	// Measurement noise covariance (z, vx, vy, vz)
-	cv::Mat1f R(4, 4);
-	R << 0.1, 0.0, 0.00, 0.00,
-		0.0, 0.1, 0.00, 0.00,
-		0.0, 0.0, 0.05, 0.00,
-		0.0, 0.0, 0.00, 0.05;
+	// Measurement noise covariance (x, y)
+	cv::Mat1f R(2, 2);
+	R << 1e-1, 0.0,
+		0.0, 1e-1;
 	kalman.measurementNoiseCov = R;
 
 	// Main loop
@@ -73,75 +89,78 @@ int main(int argc, char *argv[])
 		// Get an image
 		cv::Mat image = ardrone.getImage();
 
-		// Prediction
-		cv::Mat prediction = kalman.predict();
+		// HSV image
+		cv::Mat hsv;
+		cv::cvtColor(image, hsv, cv::COLOR_BGR2HSV_FULL);
 
-		// Altitude
-		double altitude = ardrone.getAltitude();
+		// Binalize
+		cv::Mat binalized;
+		cv::Scalar lower(minH, minS, minV);
+		cv::Scalar upper(maxH, maxS, maxV);
+		cv::inRange(hsv, lower, upper, binalized);
 
-		// Orientations
-		double roll = ardrone.getRoll();
-		double pitch = ardrone.getPitch();
-		double yaw = ardrone.getYaw();
+		// Show result
+		cv::imshow("binalized", binalized);
 
-		// Velocities
-		double vx, vy, vz;
-		double velocity = ardrone.getVelocity(&vx, &vy, &vz);
-		cv::Mat V = (cv::Mat1f(3, 1) << vx, vy, vz);
+		// De-noising
+		cv::Mat kernel = getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3));
+		cv::morphologyEx(binalized, binalized, cv::MORPH_CLOSE, kernel);
+		//cv::imshow("morphologyEx", binalized);
 
-		// Rotation matrices
-		cv::Mat RZ = (cv::Mat1f(3, 3) << cos(yaw), -sin(yaw), 0.0,
-			sin(yaw), cos(yaw), 0.0,
-			0.0, 0.0, 1.0);
-		cv::Mat RY = (cv::Mat1f(3, 3) << cos(pitch), 0.0, sin(pitch),
-			0.0, 1.0, 0.0,
-			-sin(pitch), 0.0, cos(pitch));
-		cv::Mat RX = (cv::Mat1f(3, 3) << 1.0, 0.0, 0.0,
-			0.0, cos(roll), -sin(roll),
-			0.0, sin(roll), cos(roll));
+		// Detect contours
+		std::vector<std::vector<cv::Point>> contours;
+		cv::findContours(binalized.clone(), contours, cv::RETR_CCOMP, cv::CHAIN_APPROX_SIMPLE);
 
-		// Time [s]
-		static int64 last = cv::getTickCount();
-		double dt = (cv::getTickCount() - last) / cv::getTickFrequency();
-		last = cv::getTickCount();
-
-		// Local movements (z, vx, vy, vz)
-		cv::Mat1f M = RZ * RY * RX * V * dt;
-		cv::Mat measurement = (cv::Mat1f(4, 1) << altitude, M(0, 0), M(1, 0), M(2, 0));
-
-		// Correction
-		cv::Mat1f estimated = kalman.correct(measurement);
-
-		// Position (x, y, z)
-		double pos[3] = { estimated(0, 0), estimated(1, 0), estimated(2, 0) };
-		std::cout << "x = " << pos[0] << "[m], " << "y = " << pos[1] << "[m], " << "z = " << pos[2] << "[m]" << std::endl;
-
-		// Take off / Landing 
-		if (key == ' ') {
-			if (ardrone.onGround()) ardrone.takeoff();
-			else                    ardrone.landing();
+		// Find the largest contour
+		int contour_index = -1;
+		double max_area = 0.0;
+		for (size_t i = 0; i < contours.size(); i++) {
+			double area = fabs(cv::contourArea(contours[i]));
+			if (area > max_area) {
+				contour_index = i;
+				max_area = area;
+			}
 		}
 
-		// Move
-		double x = 0.0, y = 0.0, z = 0.0, r = 0.0;
-		if (key == 'i' || key == CV_VK_UP)    vx = 1.0;
-		if (key == 'k' || key == CV_VK_DOWN)  vx = -1.0;
-		if (key == 'u' || key == CV_VK_LEFT)  vr = 1.0;
-		if (key == 'o' || key == CV_VK_RIGHT) vr = -1.0;
-		if (key == 'j') vy = 1.0;
-		if (key == 'l') vy = -1.0;
-		if (key == 'q') vz = 1.0;
-		if (key == 'a') vz = -1.0;
-		ardrone.move3D(x, y, z, r);
+		// Object detected
+		if (contour_index >= 0) {
+			// Moments
+			cv::Moments moments = cv::moments(contours[contour_index], true);
+			double marker_y = (int)(moments.m01 / moments.m00);
+			double marker_x = (int)(moments.m10 / moments.m00);
 
-		// Change camera
-		static int mode = 0;
-		if (key == 'c') ardrone.setCamera(++mode % 4);
+			// Measurements
+			cv::Mat measurement = (cv::Mat1f(2, 1) << marker_x, marker_y);
+
+			// Correction
+			cv::Mat estimated = kalman.correct(measurement);
+
+			// Show result
+			cv::Rect rect = cv::boundingRect(contours[contour_index]);
+			cv::rectangle(image, rect, cv::Scalar(0, 255, 0));
+		}
+
+		// Prediction
+		cv::Mat1f prediction = kalman.predict();
+		int radius = 1e+3 * kalman.errorCovPre.at<float>(0, 0);
+
+		// Show predicted position
+		cv::circle(image, cv::Point(prediction(0, 0), prediction(0, 1)), radius, cv::Scalar(0, 255, 0), 2);
 
 		// Display the image
-		cv::circle(map, cv::Point(-pos[1] * 100.0 + map.cols / 2, -pos[0] * 100.0 + map.rows / 2), 2, CV_RGB(255, 0, 0));
-		cv::imshow("map", map);
 		cv::imshow("camera", image);
+	}
+
+	// Save thresholds
+	fs.open(filename, cv::FileStorage::WRITE);
+	if (fs.isOpened()) {
+		cv::write(fs, "H_MAX", maxH);
+		cv::write(fs, "H_MIN", minH);
+		cv::write(fs, "S_MAX", maxS);
+		cv::write(fs, "S_MIN", minS);
+		cv::write(fs, "V_MAX", maxV);
+		cv::write(fs, "V_MIN", minV);
+		fs.release();
 	}
 
 	// See you
