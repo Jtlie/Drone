f39748c6cd0fdea7e9c10551569d1b65558ab45b
Jtlie <jtlie@live.nl>
2015-07-29 12:15:24 +0200

    ???

--- OpenCV2.2 tracker/OpenCV2.2 tracker/tracker.cpp
+++ OpenCV2.2 tracker/OpenCV2.2 tracker/tracker.cpp
@@ -1,109 +1,53 @@
-#include<opencv/cvaux.h>
-#include<opencv/highgui.h>
-#include<opencv/cxcore.h>
-#include<stdio.h>
-#include<stdlib.h>
+#include <iostream>
+#include <opencv2/opencv.hpp>
 
-int main(int argc, char* argv[]){
-	CvSize size640x480 = cvSize(640, 480);
+using namespace std;
+using namespace cv;
 
-	CvCapture* p_capWebcam;
+int main(int argc, const char * argv[])
+{
+	VideoCapture cap(CV_CAP_ANY);
+	cap.set(CV_CAP_PROP_FRAME_WIDTH, 320);
+	cap.set(CV_CAP_PROP_FRAME_HEIGHT, 240);
+	if (!cap.isOpened())
+		return -1;
 
-	IplImage* p_imgOriginal;
-	IplImage* p_imgProcessed;
+	Mat img;
+	HOGDescriptor hog;
+	hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
 
-	CvMemStorage* p_strStorage;
-	CvSeq* p_seqCircles;
+	namedWindow("video capture", CV_WINDOW_AUTOSIZE);
+	while (true)
+	{
+		cap >> img;
+		if (!img.data)
+			continue;
 
-	float* p_fltXYRadius;
+		vector<Rect> found, found_filtered;
+		hog.detectMultiScale(img, found, 0, Size(8, 8), Size(32, 32), 1.05, 2);
 
-	int i;
-	char charCheckForEscKey;
-
-	p_capWebcam = cvCaptureFromCAM(0);
-	
-	if (p_capWebcam == NULL){
-		printf("Geen webcam aangesloten!");
-		getchar();
-		return(-1);
-	}
-
-	cvNamedWindow( "Original"
-				  , CV_WINDOW_AUTOSIZE);
-	cvNamedWindow( "Processed"
-				  , CV_WINDOW_AUTOSIZE);
-
-	p_imgProcessed = cvCreateImage(   size640x480
-									, IPL_DEPTH_8U, 1);
-
-
-	while (1){
-		p_imgOriginal = cvQueryFrame(p_capWebcam);
-		if (p_imgOriginal == NULL) {
-			printf("Geen beeld gevonden!");
-			getchar();
-			break;
+		size_t i, j;
+		for (i = 0; i<found.size(); i++)
+		{
+			Rect r = found[i];
+			for (j = 0; j<found.size(); j++)
+				if (j != i && (r & found[j]) == r)
+					break;
+			if (j == found.size())
+				found_filtered.push_back(r);
 		}
-
-		cvInRangeS(   
-			p_imgOriginal
-		  , CV_RGB(175, 0, 0)
-		  , CV_RGB(256, 100, 100)
-		  , p_imgProcessed);
-
-		p_strStorage = cvCreateMemStorage(0);
-
-		cvSmooth(p_imgProcessed
-			, p_imgProcessed
-			, CV_GAUSSIAN
-			, 9
-			, 9);
-
-		p_seqCircles = cvHoughCircles(
-			  p_imgProcessed
-			, p_strStorage
-			, CV_HOUGH_GRADIENT
-			, 2
-			, p_imgProcessed->height / 4
-			, 100
-			, 50
-			, 10
-			, 400);
-		
-		for (i = 0; i < p_seqCircles->total; i++){
-			p_fltXYRadius = (float*)cvGetSeqElem(p_seqCircles, i);
-
-			printf("ball positie x = %f, y = %f, r = %f \n", p_fltXYRadius[0]
-														   , p_fltXYRadius[1]
-														   , p_fltXYRadius[2]);
-			cvCircle(
-				  p_imgOriginal
-				, cvPoint(cvRound(p_fltXYRadius[0])
-				, cvRound(p_fltXYRadius[1]))
-				, 3
-				, CV_RGB(0, 255, 0)
-				, CV_FILLED);
-
-			cvCircle(
-				p_imgOriginal
-				, cvPoint(cvRound(p_fltXYRadius[0])
-				, cvRound(p_fltXYRadius[1]))
-				, cvRound(p_fltXYRadius[2])
-				, CV_RGB(255, 0, 0)
-				, 3);
-			}
-		cvShowImage("Original", p_imgOriginal);
-		cvShowImage("Processed", p_imgProcessed);
-
-		cvReleaseMemStorage(&p_strStorage);
-
-		charCheckForEscKey = cvWaitKey(10);
-		if (charCheckForEscKey == 27) break;
+		for (i = 0; i<found_filtered.size(); i++)
+		{
+			Rect r = found_filtered[i];
+			r.x += cvRound(r.width*0.1);
+			r.width = cvRound(r.width*0.8);
+			r.y += cvRound(r.height*0.06);
+			r.height = cvRound(r.height*0.9);
+			rectangle(img, r.tl(), r.br(), cv::Scalar(0, 255, 0), 2);
+		}
+		imshow("video capture", img);
+		if (waitKey(20) >= 0)
+			break;
 	}
-	cvReleaseCapture(&p_capWebcam);
-	cvDestroyWindow("Original");
-	cvDestroyWindow("Processed");
-
-	return(0);
-
+	return 0;
 }
